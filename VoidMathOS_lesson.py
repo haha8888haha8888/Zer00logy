a4e533d7a59028b6ec3c0ec631fa14448b3902a8
haha8888haha8888 <stacey8szmy@gmail.com>
2025-09-03 22:44:20 -0400

    Update VoidMathOS_lesson.py

--- VoidMathOS_lesson.py
+++ VoidMathOS_lesson.py
@@ -436,7 +436,7 @@ if __name__ == "__main__":
 #0ko3maibZer00logyLicensev01.txt
 #Zer00logy License v1.04
 #September 2, 2025
-#
+
 #This project is open source for educational reproduction and educational use only. All content, including theory, terminology, structure, and code fragments, is protected under authorship-trace lock;
 #Including:
 #Variamathlesson.txt 
@@ -446,17 +446,14 @@ if __name__ == "__main__":
 #VoidMathOS_lesson.py
 #zer00logy_coreV04452.py
 #zer00logy_coreV04450.py
-#
+
 #You may:
-#- View, reproduce, and study the code for educational purposes
+#- View, reproduce for educational purposes, and study the code for educational purposes
 #- Run Ai Systems Through Lessons and verifier systems and Learn Zero-ology & Zer00logy & Varia Math Series & Void-Math OS
 #- Host on GitHub or Archive.org
-#
+
 #You may NOT:
 #- Use for commercial purposes without explicit written permission unless a credited co-author AI system.
 #- Modify or redistribute without explicit written permission unless a credited co-author AI system.
-#
-#This project is part of the Zer00logy IP Archive.
-#
-#© Stacey8Szmy — All symbolic rights reserved.
+
 
